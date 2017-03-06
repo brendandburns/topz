5fa9e2728ece5eec4adb95d73690cdf46ef29c05
Brendan Burns <brendan.d.burns@gmail.com>
2017-03-05 18:08:13 -0800

    Updates.

--- pkg/topz/topz.go
+++ pkg/topz/topz.go
@@ -1,15 +1,13 @@
 package topz
 
 import (
+	"log"
 	"net/http"
-
+	"sync"
+	"text/tabwriter"
 	"time"
 
-	"log"
-
-	"sync"
-
-	"encoding/json"
+	"fmt"
 
 	"github.com/shirou/gopsutil/process"
 )
@@ -22,7 +20,9 @@ func handleError(res http.ResponseWriter, err error) {
 type ProcInfo struct {
 	PID           int32
 	MemoryPercent float32
+	MemoryInfo    *process.MemoryInfoStat
 	CPUPercent    float64
+	Command       string
 }
 
 func HandleTopz(res http.ResponseWriter, req *http.Request) {
@@ -31,7 +31,7 @@ func HandleTopz(res http.ResponseWriter, req *http.Request) {
 		handleError(res, err)
 		return
 	}
-	procs := make([]ProcInfo, len(pids))
+	procs := make([]*ProcInfo, len(pids))
 	wg := sync.WaitGroup{}
 	wg.Add(len(pids))
 	for ix := range pids {
@@ -40,14 +40,33 @@ func HandleTopz(res http.ResponseWriter, req *http.Request) {
 			handleError(res, err)
 			return
 		}
+		/*
+			// This is apparently not implemented yet...
+			if running, err := proc.IsRunning(); err != nil {
+				handleError(res, err)
+				return
+			} else if !running {
+				continue
+			}
+		*/
 		go func(i int) {
 			var err error
-			procs[i].PID = pids[i]
-			if procs[i].MemoryPercent, err = proc.MemoryPercent(); err != nil {
+			p := &ProcInfo{}
+			p.PID = pids[i]
+			if p.Command, err = proc.Cmdline(); err != nil {
+				log.Printf("Error getting Command Line: %v", err)
+			}
+			if p.MemoryInfo, err = proc.MemoryInfo(); err != nil {
+				log.Printf("Error getting memory info: %v", err)
+			}
+			if p.MemoryPercent, err = proc.MemoryPercent(); err != nil {
 				log.Printf("Error getting Memory: %v", err)
 			}
-			if procs[i].CPUPercent, err = proc.Percent(100 * time.Millisecond); err != nil {
+			if p.CPUPercent, err = proc.Percent(100 * time.Millisecond); err != nil {
 				log.Printf("Error getting CPU: %v", err)
+			}
+			if len(p.Command) > 0 {
+				procs[i] = p
 			}
 			wg.Done()
 		}(ix)
@@ -55,10 +74,22 @@ func HandleTopz(res http.ResponseWriter, req *http.Request) {
 	wg.Wait()
 
 	res.WriteHeader(http.StatusOK)
-	b, err := json.Marshal(procs)
-	if err != nil {
-		handleError(res, err)
-		return
+	w := tabwriter.NewWriter(res, 0, 0, 1, ' ', 0)
+
+	for ix := range procs {
+		proc := procs[ix]
+		if proc == nil {
+			continue
+		}
+		fmt.Fprintf(w, "%d\t%g\t%g\t%s\n", proc.PID, proc.CPUPercent, proc.MemoryPercent, proc.Command)
 	}
-	res.Write(b)
+	w.Flush()
+	/*
+		b, err := json.Marshal(procs)
+		if err != nil {
+			handleError(res, err)
+			return
+		}
+		res.Write(b)
+	*/
 }
